66dba443b97d9f44060b7734092a10a7881f967f
koryou <90591869+k0ryou@users.noreply.github.com>
2021-12-29 17:49:39 +0900

    add comments

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # FaceMosaic
 ## アプリ使用上の注意
-- Chrome上でしか、動作確認を行えておりません
+- Chrome上で動作確認を行っております
 - このプログラムを使用するためには、Chrome://flagsの「Experimental Web Platform features」をEnabledに変更する必要があります
+- 現在はjpeg画像のみに対応しています
--- js/script.js
+++ js/script.js
@@ -7,76 +7,113 @@ let app = new Vue({
             selectedImageWidht: 0,
             isImageLoad: true,
             ctxImageData: "",
-            canvas: "",
             sliderValue: 0,
+            canvas: "",
+            ctx: {},
         }
     },
+    mounted() {// アプリ起動時の処理 
+        this.canvas = this.$refs.canvas;
+        this.ctx = this.canvas.getContext('2d');
+    },
     methods: {
+        /**
+         * 選択された画像を取得
+         */
         getInputFile() {
             const selectedFiles = this.$refs.file;
             const selectedFile = selectedFiles.files[0];
-            
+  
             this.showInputFile(selectedFile);
         },
+        /**
+         * 選択された画像を表示
+         * @param {Object} file - 取得した画像ファイル
+         */
         showInputFile(file) {
             this.selectedImage = window.URL.createObjectURL(file);
             this.isImageLoad = false;
         },
+        /**
+         * 画像内の顔を認識して、モザイクをかける
+         * @returns 
+         */
         detectFaces() {
-            if(window.FaceDetector == undefined) {
+            if(window.FaceDetector === undefined) {// ブラウザがサポートしていない場合、終了
                 console.log("Not Supported")
                 return;
             }
             
+            // imgタグを取得
             const image = this.$refs.image;
-            const canvas = this.$refs.canvas;
-            const ctx = canvas.getContext('2d');
 
-            canvas.height = image.naturalHeight;
-            canvas.width = image.naturalWidth;
+            // canvasの大きさを画像の大きさに合わせる
+            this.canvas.height = image.naturalHeight;
+            this.canvas.width = image.naturalWidth;
 
-            ctx.drawImage(image, 0, 0);
+            // contextに画像を描写
+            this.ctx.drawImage(image, 0, 0);
             
+            // スライダーの値が0の時はモザイクをかける必要はないため、終了
             if(Number(this.sliderValue) === 0) return;
 
-            this.ctxImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            // contextに描画された画像データを取得
+            this.ctxImageData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
 
+            // FaceDetectorAPIによる顔認識処理
             const faceDetector = new window.FaceDetector();
             faceDetector.detect(image).then((faces) => {
-                faces.forEach((face) => {
-                    let faceValue = face.boundingBox, mosaicSize = Number(this.sliderValue);
-                    console.log(faceValue)
+
+                faces.forEach((face) => {// 認識した全ての顔に以下の処理を行う
+                    let faceValue = face.boundingBox; // 画像内の顔情報を取得(画像内の顔の位置など)
+                    let mosaicSize = Number(this.sliderValue);
+                    
+                    // 取得した顔の位置情報を基に、モザイクをかける
                     for(let y = faceValue.top; y <= faceValue.bottom-mosaicSize; y += mosaicSize) {
                         for(let x = faceValue.left; x <= faceValue.right-mosaicSize; x += mosaicSize) {
                             this.createMosaic(x, y, mosaicSize, mosaicSize);
                         }
                     }
                 })
+
             }).catch((e) => {
                 console.log(e);
             })
         },
+        /**
+         * 画像にモザイクをかける
+         * @param {number} x - モザイクをかける始点(x方向)
+         * @param {number} y - モザイクをかける始点(y方向)
+         * @param {number} w - モザイクの幅
+         * @param {number} h - モザイクの高さ
+         */
         createMosaic(x, y, w, h) {
-            let ctx = this.$refs.canvas.getContext('2d')
             let r = 0, g = 0, b = 0;
             
-            let src = ctx.getImageData(x, y, w, h);
-            let dst = ctx.createImageData(w, h)
+            let src = this.ctx.getImageData(x, y, w, h); // contextの画像データ
+            let dst = this.ctx.createImageData(w, h) // サイズw x hのモザイク本体
 
+            /*
+                RGB値の総和を求める
+                src.dataには、選択した範囲の画像データがR,G,B,A(透明度）の順番に、選択した画像データの長さだけ並んでいるため、+=4をしている
+            */
             for (let i = 0; i < src.data.length; i += 4) {
                 r += src.data[i]
                 g += src.data[i + 1]
                 b += src.data[i + 2]
             }
 
-            r /= src.data.length / 4
-            g /= src.data.length / 4
-            b /= src.data.length / 4
+            // RGB値の各値の平均値を求める
+            r /= (src.data.length / 4)
+            g /= (src.data.length / 4)
+            b /= (src.data.length / 4)
 
+            // 整数値に変換
             r = Math.ceil(r)
             g = Math.ceil(g)
             b = Math.ceil(b)
-
+            
+            // モザイクのrgb値と透明度を決定
             for (let i = 0; i < src.data.length; i += 4) {
                 dst.data[i] = r
                 dst.data[i + 1] = g
@@ -84,12 +121,15 @@ let app = new Vue({
                 dst.data[i + 3] = 255
             }
 
-            ctx.putImageData(dst, x, y)
+            // contextにモザイクを描写
+            this.ctx.putImageData(dst, x, y)
         },
+        /**
+         * モザイクをかけた画像データをダウンロードする
+         */
         downloadResultImage() {
-            const canvas = this.$refs.canvas;
-            let base64 = canvas.toDataURL("image/jpeg");
-            this.$refs.download.href = base64;
+            let base64 = this.canvas.toDataURL("image/jpeg");
+            this.$refs.download.href = base64; // aタグのリンクにbase64で暗号化した画像データをのせる
         }
     },
 })

d5c46a5c3c5069b93bde2d7b513d45574765d34d
koryou <90591869+k0ryou@users.noreply.github.com>
2021-12-29 16:12:45 +0900

    add some functions

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FaceMosaic
+## アプリ使用上の注意
+- Chrome上でしか、動作確認を行えておりません
+- このプログラムを使用するためには、Chrome://flagsの「Experimental Web Platform features」をEnabledに変更する必要があります
--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@ let app = new Vue({
     data() {
         return {
             selectedImage: "",
+            selectedImageHeight: 0,
+            selectedImageWidht: 0,
             isImageLoad: true,
+            ctxImageData: "",
+            canvas: "",
+            sliderValue: 0,
         }
-    },
-    created() {
-
     },
     methods: {
         getInputFile() {
@@ -25,17 +27,69 @@ let app = new Vue({
                 console.log("Not Supported")
                 return;
             }
-        
-            const faceDetector = new window.FaceDetector();
+            
             const image = this.$refs.image;
+            const canvas = this.$refs.canvas;
+            const ctx = canvas.getContext('2d');
+
+            canvas.height = image.naturalHeight;
+            canvas.width = image.naturalWidth;
+
+            ctx.drawImage(image, 0, 0);
+            
+            if(Number(this.sliderValue) === 0) return;
+
+            this.ctxImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+            const faceDetector = new window.FaceDetector();
             faceDetector.detect(image).then((faces) => {
-                console.log(faces)
-                for(let face in faces) {
-                    console.log(face);
-                }
+                faces.forEach((face) => {
+                    let faceValue = face.boundingBox, mosaicSize = Number(this.sliderValue);
+                    console.log(faceValue)
+                    for(let y = faceValue.top; y <= faceValue.bottom-mosaicSize; y += mosaicSize) {
+                        for(let x = faceValue.left; x <= faceValue.right-mosaicSize; x += mosaicSize) {
+                            this.createMosaic(x, y, mosaicSize, mosaicSize);
+                        }
+                    }
+                })
             }).catch((e) => {
                 console.log(e);
             })
+        },
+        createMosaic(x, y, w, h) {
+            let ctx = this.$refs.canvas.getContext('2d')
+            let r = 0, g = 0, b = 0;
+            
+            let src = ctx.getImageData(x, y, w, h);
+            let dst = ctx.createImageData(w, h)
+
+            for (let i = 0; i < src.data.length; i += 4) {
+                r += src.data[i]
+                g += src.data[i + 1]
+                b += src.data[i + 2]
+            }
+
+            r /= src.data.length / 4
+            g /= src.data.length / 4
+            b /= src.data.length / 4
+
+            r = Math.ceil(r)
+            g = Math.ceil(g)
+            b = Math.ceil(b)
+
+            for (let i = 0; i < src.data.length; i += 4) {
+                dst.data[i] = r
+                dst.data[i + 1] = g
+                dst.data[i + 2] = b
+                dst.data[i + 3] = 255
+            }
+
+            ctx.putImageData(dst, x, y)
+        },
+        downloadResultImage() {
+            const canvas = this.$refs.canvas;
+            let base64 = canvas.toDataURL("image/jpeg");
+            this.$refs.download.href = base64;
         }
     },
 })
